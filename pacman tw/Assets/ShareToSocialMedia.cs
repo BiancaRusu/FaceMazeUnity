0d9b79e27ed71fd57483a84fbf2eb110d17c26c9
bia <bianca.rusu_zamfirache@info.uaic.ro>
2016-05-31 12:57:58 +0300

    Revert "Revert "share twitter""
    This reverts commit bd12d6a7b87ee7c0eafd2a62796103c9e63775e3.

---
+++ pacman tw/Assets/ShareToSocialMedia.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShareToSocialMedia : MonoBehaviour {
+
+    private const string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+    private const string TWEET_LANGUAGE = "en";
+
+    public void  ShareToTwitter()
+    {
+        Application.OpenURL(TWITTER_ADDRESS +
+                    "?text=" + "Jucand FaceMaze" +
+                    "&amp;lang=" + WWW.EscapeURL(TWEET_LANGUAGE));
+    }
+}
